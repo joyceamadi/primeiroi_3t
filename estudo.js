cb9f9bef46721f43e9f8c78dbbfa7879fc64aa40
joyceamadi <110128745+joyceamadi@users.noreply.github.com>
2022-09-28 16:57:16 -0300

    add tabuada

--- estudo.js
+++ estudo.js
@@ -1,1 +1,16 @@
-
+// alert("Oi! Eu sou o Alert");
+// document.write("Estou na página.");
+// console.log("Eu estou no console.");
+let tabuada = 2;
+function alo(){
+    document.write(<h1>Tabuada document "  +  tabuada + "</h
+    document.write(tabuada + " x 1 = ")+ (tabuada *);
+    document.write(tabuada + " x 2 = ")+ (tabuada *);
+    document.write(tabuada + " x 3 = ")+ (tabuada *);
+    document.write(tabuada + " x 4 = ")+ (tabuada *);
+    document.write(tabuada + " x 5 = ")+ (tabuada *);
+    document.write(tabuada + " x 6 = ")+ (tabuada *);
+    document.write(tabuada + " x 7 = ")+ (tabuada *);
+    document.write(tabuada + " x 8 = ")+ (tabuada *);
+    document.write(tabuada + " x 9 = ")+ (tabuada *);
+    document.write(tabuada + " x 10 = ")+ (tabuada *);
